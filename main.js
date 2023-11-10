77de420600f1f20d0b066a82fcf44435119d5b1e
Amanda <amanda.ausejo@gmail.com>
2023-11-10 12:21:56 +0100

    feat esercizio 3

--- main.js
+++ main.js
@@ -1,38 +1,54 @@
 //-------------
 //Esercizio 2
 //-------------
-const riempiArray = (numeroElementi, valoreMinimo, valoreMassimo) => {
-    const array = [];
+// const riempiArray = (numeroElementi, valoreMinimo, valoreMassimo) => {
+//     const array = [];
 
-    for (let i = 0; i < numeroElementi; i++) {
-        const numeroCasuale = Math.floor(
-            Math.random() * (valoreMassimo - valoreMinimo + 1) + valoreMinimo
-        );
-        array.push(numeroCasuale);
-    }
-    return array;
-};
+//     for (let i = 0; i < numeroElementi; i++) {
+//         const numeroCasuale = Math.floor(
+//             Math.random() * (valoreMassimo - valoreMinimo + 1) + valoreMinimo
+//         );
+//         array.push(numeroCasuale);
+//     }
+//     return array;
+// };
 
 // let risultato = riempiArray(10, 5, 10);
 // console.log(risultato);
 
-const contaPariDispari = (array) => {
-    let pariDispari = { pari: 0, dispari: 0 };
-    console.log(array);
-    for (let i = 0; i < array.length; i++) {
-        let arrayI = array[i];
-        if (arrayI % 2 === 0) {
-            pariDispari.pari += 1;
-        } else if (arrayI % 2 !== 0) {
-            pariDispari.dispari += 1;
+// const contaPariDispari = (array) => {
+//     let pariDispari = { pari: 0, dispari: 0 };
+//     console.log(array);
+//     for (let i = 0; i < array.length; i++) {
+//         let arrayI = array[i];
+//         if (arrayI % 2 === 0) {
+//             pariDispari.pari += 1;
+//         } else if (arrayI % 2 !== 0) {
+//             pariDispari.dispari += 1;
+//         }
+//     }
+//     return pariDispari;
+// };
+
+// let conteggio = contaPariDispari(riempiArray(10, 5, 10));
+// console.log(conteggio);
+
+//-------------
+//Esercizio 3
+//-------------
+
+window.addEventListener("load", () => {
+    const body = document.body;
+    const bottone = document.getElementById("bottoneInvio");
+    bottone.addEventListener("click", () => {
+        const input = document.getElementById("testoUtente");
+        const valoreInput = input.value;
+        if (valoreInput === "") {
+            alert("Inserisci del testo prima di inviare");
+        } else {
+            const creaP = document.createElement("p");
+            creaP.innerHTML = `questo e' il valore inserito: ${valoreInput}`;
+            body.appendChild(creaP);
         }
-    }
-    return pariDispari;
-};
-
-let conteggio = contaPariDispari(riempiArray(10, 5, 10));
-console.log(conteggio);
-
-//-------------
-//Esercizio 1
-//-------------
+    });
+});
